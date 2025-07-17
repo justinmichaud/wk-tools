1c19b8c900a02787784a05f54f4e588ffec024f3
Justin Michaud <jmichaud@igalia.com>
2025-07-17 15:48:49 -0600

    .

--- helix/hx-open.sh
+++ helix/hx-open.sh
@@ -6,14 +6,16 @@ then
   exit 1
 fi
 
-file_path=$(echo "$1" | sed 's/.*Source\///')
-line_num=$2
+file_path=$(echo "$1" | sed 's/.*Source\///' | sed 's/:.*//')
+line_num=$(echo "$1" | grep -Po '(?<=:).*')
 window_title="^KittyHelix"
 
-kitty @ send-text --match title:$window_title '\E'
+notify-send "Got $file_path line $line_num"
+
+kitty @ $KITTYHELIX send-text --match title:$window_title '\E'
 sleep 0.1
-kitty @ send-text --match title:$window_title ' f'
-kitty @ send-text --match title:$window_title "$file_path"
-kitty @ focus-tab --match title:$window_title
+kitty @ $KITTYHELIX send-text --match title:$window_title ' f'
+kitty @ $KITTYHELIX send-text --match title:$window_title "$file_path"
+kitty @ $KITTYHELIX focus-tab --match title:$window_title
 sleep 0.3
-kitty @ send-text --match title:$window_title "\rg${line_num}g"
+kitty @ $KITTYHELIX send-text --match title:$window_title "\rg${line_num}g"
