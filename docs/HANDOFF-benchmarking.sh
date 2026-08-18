fe932eb9a09c48c284dd1e7e64c0ebb3376c0235
Justin Michaud <jmichaud@igalia.com>
2026-08-18 15:24:17 -0600

    .

--- docs/HANDOFF-benchmarking.sh
+++ docs/HANDOFF-benchmarking.sh
@@ -9,3 +9,5 @@ THis should work for macOS and linux, plus rpi5 (ubuntu), plus rpi5/4/3 (yocto).
 Do research on what speed and size drive will be required. Ideally a 32gb flash drive would work, or the bmc image boot option for moose (there should be more than enough ram).
 
 Netboot is another possibility that could work.
+
+*** THis should supersede the previous discussion about rpi5. rpi5 should retain full privileges just like any other workstation, but when used for benchmarking, should netboot into a special benchmarking image, and should use podman for sandboxing claude just like moose. ***
